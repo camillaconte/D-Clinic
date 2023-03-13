b65411533b9f0ab588259efff28d83960a971915
camillaconte <118888603+camillaconte@users.noreply.github.com>
2023-03-13 19:23:25 +0100

    first commit

---
+++ src/main/java/develhope/DClinic/DClinicApplication.java
@@ -0,0 +1,13 @@
+package develhope.DClinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DClinicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/develhope/DClinic/DClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package develhope.DClinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
