158943c987603960b27baaae42da771fb08456f9
mirko <mirkmarkese@gmail.com>
2023-04-08 11:32:44 +0200

    basic patient structor1

--- src/main/java/develhope/DClinic/domain/Patient.java
+++ src/main/java/develhope/DClinic/domain/Patient.java
@@ -1,6 +1,7 @@
 package develhope.DClinic.domain;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.*;
 
 import java.util.List;
@@ -23,6 +24,7 @@ public class Patient {
     @Column(nullable = false)
     private String firstName;
     @Column(nullable = false)
+
     private String lastName;
 
     @Column(name = "fiscalCode")
