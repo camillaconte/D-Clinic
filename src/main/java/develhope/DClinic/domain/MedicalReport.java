deaf0d912d87e2613b224296cfcd772de0df83d5
camillaconte <118888603+camillaconte@users.noreply.github.com>
2023-04-12 10:44:32 +0200

    modified MedicalReport id annotation, added allocationSize to SequenceGenerator

--- src/main/java/develhope/DClinic/domain/MedicalReport.java
+++ src/main/java/develhope/DClinic/domain/MedicalReport.java
@@ -17,7 +17,8 @@ public class MedicalReport {
     @Id
     @SequenceGenerator(
             name = "medicalReports_id_sequence",
-            sequenceName = "medicalReports_id_sequence"
+            sequenceName = "medicalReports_id_sequence",
+            allocationSize = 1
     )
     @GeneratedValue(
             strategy = GenerationType.SEQUENCE,
